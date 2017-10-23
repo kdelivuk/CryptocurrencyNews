da00e911fc6089c142e6d9e3bd4ffea3abdfdc39
Kristijan Delivuk <kristijan.delivuk@decode.agency>
2017-10-23 12:48:18 +0200

    defining main coordinator class

--- CryptocurrencyNews/Presentation/Coordinators/Coordinator.swift
+++ CryptocurrencyNews/Presentation/Coordinators/Coordinator.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2017 Kristijan Delivuk. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Coordinator: NSObject {
+    
+    let rootViewController: UIViewController
+    
+    var shouldEnd: (() -> Void)?
+    
+    private(set) var coordinators = [Coordinator]()
+    
+    init(rootViewController: UIViewController) {
+        self.rootViewController = rootViewController
+        self.coordinators = []
+    }
+    
+    func push(childCoordinator: Coordinator) {
+        coordinators.append(childCoordinator)
+        childCoordinator.start()
+    }
+    
+    @discardableResult
+    func pop(childCoordinator: Coordinator) -> Coordinator? {
+        if let index = coordinators.index(of: childCoordinator) {
+            childCoordinator.coordinatorWillEnd()
+            return coordinators.remove(at: index)
+        } else {
+            return nil
+        }
+    }
+    
+    func start() {
+        // To be overriden
+    }
+    
+    
+    /// This method will be called before coordinator is removed from the stack.
+    /// Use this method to clean view hierarchy
+    func coordinatorWillEnd() {
+        
+    }
+}
