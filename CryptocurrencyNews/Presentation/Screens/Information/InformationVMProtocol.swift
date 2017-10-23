cd489e702ab90964703f1fec1d7110dee6ceaad3
Kristijan Delivuk <kristijan.delivuk@decode.agency>
2017-10-23 10:19:47 +0200

    adding basic project structure

---
+++ CryptocurrencyNews/Presentation/Screens/Information/InformationVMProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  InformationVMProtocol.swift
+//  CryptocurrencyNews
+//
+//  Created by Kristijan Delivuk on 23/10/2017.
+//  Copyright © 2017 Kristijan Delivuk. All rights reserved.
+//
+
+import Foundation
